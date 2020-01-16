407a5453d147fcfb0b9d136c61bfdb67545e06de
Shauna Sapper <shaunasapper@gmail.com>
2020-01-16 10:47:47 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -7,3 +7,11 @@ function initializePage() {
             $('.jumbotron h1').text("Javascript is connected");
             });
 }
+
+$("a.thumbnail").click(projectClick);
+
+function projectClick(e) {
+  //prevent the page from reloading
+  e.preventDefault();
+  $(this).css("background-color", "#f2ebe9");
+}
