5ef608a2c3e1946f901ea145b6795d2cb60dc03e
Shauna Sapper <shaunasapper@gmail.com>
2020-01-16 11:25:59 -0800

    More styling for stretch goal

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,17 +1,19 @@
+var colors = ['#027373', '#014040', '#038C7F', '#AABFB9'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+
 $(document).ready(function() {
     initializePage();
 });
 
 function initializePage() {
     $('#testjs').click(function(e) {
-            $('.jumbotron h1').text("Javascript is connected");
+            $('.jumbotron h1').css('color', random_color);
             });
 }
 
 $("a.thumbnail").click(projectClick);
-
 function projectClick(e) {
   //prevent the page from reloading
   e.preventDefault();
-  $(this).css("background-color", "#f2ebe9");
+  $(this).css('background-color', '#AABFB9');
 }
